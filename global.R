ee3d3659c55d19a160aa4ef27c6b9e58a11da99d
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-07-09 10:58:23 +0100

    Clean-up
    * Annotate package calls
    
    * Improve commenting
    
    * Remove unused functionality

--- global.R
+++ global.R
@@ -1,39 +1,48 @@
-library(shiny)
-library(shinyWidgets)
-library(shinyjs)
-library(shinyBS)
-library(shinyFeedback)
-library(shinyanimate)
-library(shinythemes)
-library(shinycssloaders)
-library(bsplus)
-library(jsonlite)
-library(RCurl) # for checking whether url.exists
-library(digest)
-library(knitr)
-library(markdown)
-library(dplyr)
-library(flextable)
-library(tidyr)
-library(officer)
+
+# PACKAGE CALLS -----------------------------------------------------------
+
+library(shiny) # Web Application Framework for R
+library(shinyWidgets) # Custom Inputs Widgets for Shiny
+library(shinyjs) # Easily Improve the User Experience of Your Shiny Apps in Seconds
+library(shinyBS) # Twitter Bootstrap Components for Shiny
+library(shinyFeedback) # Display User Feedback in Shiny Apps
+library(shinyanimate) # Animation for 'shiny' Elements
+library(shinythemes) # Themes for Shiny
+library(shinycssloaders) # Add CSS Loading Animations to 'shiny' Outputs
+library(bsplus) # Adds Functionality to the R Markdown + Shiny Bootstrap Framework
+library(jsonlite) # A Robust, High Performance JSON Parser and Generator for R
+library(RCurl) # General Network (HTTP/FTP/...) Client Interface for R 
+library(digest) # Create Compact Hash Digests of R Objects
+library(knitr) # A General-Purpose Package for Dynamic Report Generation in R
+library(markdown) # Render Markdown with the C Library 'Sundown'
+library(dplyr) # A Grammar of Data Manipulation
+library(flextable) # Functions for Tabular Reporting
+library(tidyr) # Tidy Messy Data
+library(officer) # Manipulation of Microsoft Word and PowerPoint Documents
+library(kableExtra) # Construct Complex Table with 'kable' and Pipe Syntax
+
+
+# SOURCE LOCAL SCRIPTS ----------------------------------------------------
 
 source("R/helpers.R")
 source("R/validation.R")
-source("R/renderPDF.R")
+
+# PRELIMINARY SET-UP ------------------------------------------------------
 
 # First, we load the .json, which defines the structure of the application
 questions <- jsonlite::read_json(path = "data/questions.json")
 answers <- jsonlite::read_json(path = "data/answers.json")
 
-df_m <- data.table::rbindlist(questions$Sections$`PRISMA MAIN CHECKLIST`$Questions, fill=TRUE) %>%
+# Convert Main questions from JSON to dataframe for use in the exports
+df_m <- data.table::rbindlist(questions$Sections$`PRISMA MAIN CHECKLIST`$Questions, fill = TRUE) %>%
   filter(Type != "break") %>%
   select(Domain, Qnumber, Label) %>%
   mutate(Domain = ifelse(Domain == "",NA, Domain)) %>%
-  fill(Domain) %>%
   mutate(seq = seq(1:nrow(.)))
 
 colnames(df_m)[2] <- "No"
 
+# Convert Abstract questions from JSON to dataframe for use in the exports
 df_a <- data.table::rbindlist(questions$Sections$ABSTRACT$Questions, fill=TRUE) %>%
   filter(Type != "break") %>%
   select(Domain, Qnumber, Label) %>%
@@ -47,8 +56,7 @@ headList <- questions$Head
 sectionsList <- questions$Sections
 answerList <- answers$Answers
 
-# Name the questions (ind_1 ... ind_n) - this slightly reduces the tedious filling in of question numbers in .json
-# and reduces the likelihood of a manual mistake.
+# Name the questions (ind_m_1 ... ind_m_n) 
 sectionsList <- lapply(sectionsList, function(Sec){
   Sec$Questions <- lapply(Sec$Questions,  function(x,arg1) {
 
@@ -66,10 +74,7 @@ sectionsList <- lapply(sectionsList, function(Sec){
 })
 
 
-# write html code for sections prior opening the app
+# Produce the HTML code for the checklists prior to opening app
 sectionsHTML <- lapply(sectionsList, renderSection)
 names(sectionsHTML) <- NULL
 sectionsHTML <- do.call(tabsetPanel, c(sectionsHTML, id = "sections"))
-
-# write html code for heading
-headHTML <- lapply(headList, switchButtons)
